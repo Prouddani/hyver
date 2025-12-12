297b96b441f69f9fe9484b18141559381e919347
Prouddani <daniel.lintz@icloud.com>
2025-12-12 12:02:25 -0300

    Added html and minor changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const url = require('url');
 
-const { Client, IntentsBitField } = require('discord.js');
+const { } = require ('./commands.js');
+const { OAuth2Handler } = require('./oauth2.js');
+const { Client, IntentsBitField, ActionRowBuilder, ButtonStyle } = require('discord.js');
+const { ButtonBuilder } = require('@discordjs/builders');
+const { verify } = require('crypto');
 
 // Bot's client instance
 const client = new Client({
@@ -12,19 +16,60 @@ const client = new Client({
         IntentsBitField.Flags.MessageContent,
     ]
 });
+let guildId;
 
-client.on('ready', (c) => {
-    console.log(`Bot initialized as ${c.user.tag}`);
+const oauth2 = new OAuth2Handler();
+oauth2.on('redirected', async (tokens_body) => {
+    console.log(tokens_body);
+
+    const {
+        tokens_body: refreshed_tokens_body,
+        user: user_data
+    } = await oauth2.getUserDataFromRefreshToken(tokens_body.refresh_token);
+
+    console.log(user_data);
 });
 
-client.on('messageCreate', (message) => {
-    if (message.author.bot) return; // Ignore messages from bots
-
-    if (message.content === 'ping')
+client.on('clientReady', async (c) => {
+    console.log(`Bot initialized as ${c.user.tag}`);
+    
+    try
     {
-        message.reply('ping');
+
+
+        { /* Verification Message Posting */
+            posted = false;
+            const channel = await client.channels.cache.get(process.env.VerifyChannelID);
+            if (!channel) return;
+
+            const verify_messages = await channel.messages.fetch({ limit: 100 });
+            posted_already = verify_messages.some(message => message.author.id === client.user.id);
+
+            if (!posted_already)
+            {
+                // Message and create verify button
+
+                const row = new ActionRowBuilder();
+                row.components.push(
+                    new ButtonBuilder().setCustomId('verify')
+                                        .setLabel('Verify')
+                                        .setStyle(ButtonStyle.Link)
+                                        .setURL()
+                );
+                
+                await channel.send({
+                    content: 'Verify by accessing the link below:',
+                    components: [row],
+                });
+            }
+        }
+    } catch (err) {
+        console.log(err);
     }
 });
 
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isButton()) return;
+});
 
 client.login(process.env.token); // Activates the bot
